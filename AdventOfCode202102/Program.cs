8d5258841a79e82cc1cfa6f93972f0f9d2ed8c06
Fabio Scagliola <scagliola@gmail.com>
2021-12-14 15:05:42 +0100

    Both parts of this puzzle are complete!

---
+++ AdventOfCode202102/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace com.fabioscagliola.AdventOfCode202102
+{
+    class Program
+    {
+        static void Main()
+        {
+            Regex regex = new Regex(@"(down|forward|up)\s+(\d+)");
+            MatchCollection matchCollection = regex.Matches(File.ReadAllText("Input1.txt"));
+
+            // PART 1 
+            {
+                int horizontalPosition = 0;
+                int depth = 0;
+                foreach (Match match in matchCollection)
+                {
+                    string command = match.Groups[1].Value;
+                    int units = int.Parse(match.Groups[2].Value);
+                    switch (command)
+                    {
+                        case "down":
+                            depth += units;
+                            break;
+                        case "forward":
+                            horizontalPosition += units;
+                            break;
+                        case "up":
+                            depth -= units;
+                            break;
+                    }
+                }
+                Console.WriteLine($"If you multiply your final horizontal position ({horizontalPosition}) by your final depth ({depth}) you get {horizontalPosition * depth}");
+            }
+
+            // PART 2 
+            {
+                int horizontalPosition = 0;
+                int depth = 0;
+                int aim = 0;
+                foreach (Match match in matchCollection)
+                {
+                    string command = match.Groups[1].Value;
+                    int units = int.Parse(match.Groups[2].Value);
+                    switch (command)
+                    {
+                        case "down":
+                            aim += units;
+                            break;
+                        case "forward":
+                            horizontalPosition += units;
+                            depth += units * aim;
+                            break;
+                        case "up":
+                            aim -= units;
+                            break;
+                    }
+                }
+                Console.WriteLine($"If you multiply your final horizontal position ({horizontalPosition}) by your final depth ({depth}) you get {horizontalPosition * depth}");
+            }
+        }
+
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# adventofcode.2021.2
-Advent of Code 2021 – Day 2: Dive!
+# Advent of Code 2021 – Day 2: Dive!
+
+This is how I solved [Advent of Code 2021 – Day 2: Dive!](https://adventofcode.com/2021/day/2) 
+
+The solution consists of a console application written in C# based on the Microsoft .NET Framework 4.6 
+
